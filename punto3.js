61491f0566b0695a164d12f6871ff0e240bf1eca
ElissLEE <meliortizmahone@gmail.com>
2021-04-24 23:43:41 -0500

    Listado de direccion ip, rango de direcciones y broadcast

--- punto3.js
+++ punto3.js
@@ -6,7 +6,7 @@ function llamarFuncionesPunto3(e) {
     const numBits = document.getElementsByClassName("campoTexto")[3].value;
 
     //Muestra la red principal
-    var direccionPrincipal= direccionIP;
+    var direccionPrincipal= calcularRedPrincipal();
     const direccion= document.getElementById('direccionPrincipal');
     direccion.innerHTML = direccionPrincipal;
 
@@ -28,10 +28,25 @@ function llamarFuncionesPunto3(e) {
     const numHost= document.getElementById('numHost');
     numHost.innerHTML = host;
 
- 
+    //Muestra dirrecion ip, rango de direcciones y direccion de broadcast de las subredes que se pueden usar 
+    var direccionBinaria =pasarIpABinario(direccionIP);
+    var listado= calcularIpSubredes(direccionBinaria, numBits,numeroSubredes);
+    const listadoDirecciones= document.getElementById('listDirecciones');
+    listadoDirecciones.innerHTML = listado;
  
 
 }
+    // Funcion que calcula la red principal de una direccion ip 
+    function calcularRedPrincipal(){
+
+        var direccionBinario =pasarIpABinario(direccionIP);
+
+        var direccionRecortada= direccionBinario.substring(0,mascara);
+    
+        var principalBin= completarDireccion(direccionRecortada,0)
+        
+        return  binarioAIpDecimal (principalBin)
+    }
 
     //Funcion que calcula que el numero de subredees que se pueden asignar 
     function calcularSubredes (numeroBits){
@@ -61,3 +76,104 @@ function llamarFuncionesPunto3(e) {
         return cont;
     }
 
+
+    //Funcion que lista la dirrecion ip, rango de direcciones y direccion de broadcast de las subredes que se pueden usar 
+    function calcularIpSubredes(direccionIpBinaria, bitSubredes, numSubredes) {
+ 
+        var direccionAux = direccionIpBinaria.replaceAll('.', '');
+        let arregloDireccion = direccionAux.split("");
+        var mascaraAdaptada = mascara + bitSubredes;
+        var cadena = "";
+     
+        for (var i = 1; i <= numSubredes; i++) {
+            
+            cadena +=  "SUBRED  " + i + ":<br>" + "    ";
+         
+            var numSubred = calcularIpSubred(arregloDireccion, bitSubredes, i);           
+             
+            var direccion = completarDireccion(numSubred , 0);
+            direccion = direccion.substring(0, direccion.length - 1 ) + 1;
+            
+            var dirBroadcast = completarDireccion(numSubred, 1);
+
+            var direccionFinal =  dirBroadcast.substring(0,  dirBroadcast.length - 1 ) + 0;
+ 
+ 
+            cadena += binarioAIpDecimal( completarDireccion(numSubred , 0)) + "<br>" + "   ";
+            cadena += binarioAIpDecimal(direccion) + "<br>" + "   ";
+            cadena += binarioAIpDecimal(direccionFinal) + "<br>" + "   ";
+            cadena += binarioAIpDecimal( dirBroadcast) + "<br>" + "<br>";
+     
+        }
+     
+        return cadena;
+    }
+    
+ 
+    function calcularIpSubred(arregloDireccion, bitSubredes, numeroS) {
+ 
+        var contador = 0;
+        var tope = mascara + bitSubredes;
+
+        let arreglo = arregloDireccion;
+        let arregloAuxiliar = transformarDecimalBinario(numeroS, bitSubredes);
+      
+        for (var i = mascara; i < tope; i++) {
+            arreglo[i] = arregloAuxiliar[contador];
+            contador++;
+        }
+
+        return arreglo.join('');
+
+    }
+    
+    function completarDireccion(direccion,numero){
+
+        while(direccion.length < 32){
+            direccion += numero;
+        }
+
+        return direccion;
+    }
+
+    function transformarDecimalBinario(numero, cantBits) {
+      
+        var auxiliar = transformarDecimalBinario2(numero);
+        let arreglo = auxiliar.split("");
+        var numeroNuevo = cantBits - arreglo.length;
+        var nuevoArreglo = [];
+     
+        for (var i = 0; i < cantBits; i++) {
+            if (i < numeroNuevo) {
+                nuevoArreglo[i] = "0";
+            } else {
+     
+                nuevoArreglo[i] = arreglo[i - numeroNuevo];
+            }
+        }
+     
+        return nuevoArreglo;
+    }
+
+    function transformarDecimalBinario2(numero) {
+        
+        var auxiliar = numero.toString(2);
+        let arreglo = auxiliar.split("");
+
+        var nuevoArreglo = new Array();
+        var numNuevo = 8 - arreglo.length;
+       
+        var cadena = "";
+     
+        for (var i = 0; i < 8; i++) {
+            if (i < numNuevo) {
+                nuevoArreglo[i] = "0";
+            } else {
+     
+                nuevoArreglo[i] = arreglo[i - numNuevo];
+            }
+        }
+     
+        return nuevoArreglo.join('');
+    }
+    
