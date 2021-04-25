befca05cdb8d155e7ec02f897dbfa96067f789bb
ElissLEE <meliortizmahone@gmail.com>
2021-04-24 22:41:14 -0500

    se calculan la cantidad de subredes  y host que se pueden usar

--- punto3.js
+++ punto3.js
@@ -1,11 +1,12 @@
-
 function llamarFuncionesPunto3(e) {
+  
     e.preventDefault();
     dividirIPyMascara(3);
+   
+    const numBits = document.getElementsByClassName("campoTexto")[3].value;
 
     //Muestra la red principal
     var direccionPrincipal= direccionIP;
-    console.log(direccionPrincipal);
     const direccion= document.getElementById('direccionPrincipal');
     direccion.innerHTML = direccionPrincipal;
 
@@ -14,5 +15,49 @@ function llamarFuncionesPunto3(e) {
     const broadcastRed= document.getElementById('direccionBroadcast');
     broadcastRed.innerHTML = broadcast;
 
+    //Muestra la cantidad de subredes que se pueden asignar
+    var numeroSubredes = calcularSubredes(numBits);
+    const numSubredes= document.getElementById('numSubredes');
+    numSubredes.innerHTML = numeroSubredes;
+
+    //Muestra la cantidad de host que se pueden identificar en cada subred
+    var mascaraBinaria=convertirMascaraABinario();
+    var red= identificarRed(mascaraBinaria);
+    var cantBits = 32- red - numBits;
+    var host = calcularHostDeSubred(cantBits);
+    const numHost= document.getElementById('numHost');
+    numHost.innerHTML = host;
+
+ 
+ 
+
 }
+
+    //Funcion que calcula que el numero de subredees que se pueden asignar 
+    function calcularSubredes (numeroBits){
+    
+        var cont=0;
+        cont= Math.pow(2,numeroBits)-2;
+        return cont;
+
+    }
+
+    //Funcion que calcula la cantidad de host que se pueden identificar en cada subred
+    function  calcularHostDeSubred(numeroBits){
+         
+        var cont=0;
+        cont= Math.pow(2,numeroBits)-2;
+        return cont;
+    }
+    // Funcion que identifica la red de la direccion ip 
+    function identificarRed(mascaraBinaria) {
+        var cont = 0;
+        let otro = mascaraBinaria.split("");
+        for (var i = 0; i < otro.length; i++) {
+            if (otro[i] == "1") {
+                cont++;
+            }
+        }
+        return cont;
+    }
 
